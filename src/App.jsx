27555e5a4139715d8f5db25d63e7e1ad78f16fc6
anik <anikbhadra01@gmail.com>
2022-06-03 22:22:21 +0530

    setting the fast question dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import QuestionDynamically from '../components/QuestionDynamically'
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
-
+      <QuestionDynamically />
     </div>
   )
 }
